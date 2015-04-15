0d26a1c65912f9438075dbab0dc63c74cb5aaa58
Michal Lomnicki <michal.lomnicki@gmail.com>
2015-04-15 12:32:43 +0200

    Show some example endpoints on home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root to: 'home#index'
   namespace :api, defaults: { format: 'json' } do
     resources :leagues, only: [:index, :show] do
       resources :fixtures, only: [:index, :show] do
